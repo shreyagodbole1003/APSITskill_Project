8ddc11c10320edd493c156dcf0f62dfa909022df
Shreya Godbole <65806185+shreyagodbole1003@users.noreply.github.com>
2020-07-29 16:38:53 +0530

    Create Part_5.py

---
+++ Part_5.py
@@ -0,0 +1,9 @@
+#To check if the given string is Palindrome.  
+def is_palindrome(s):
+    if len(s) < 1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
