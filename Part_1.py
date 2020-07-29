7e49c5990baf6ac59edd6c6bd5daeb15953670c7
Shreya Godbole <65806185+shreyagodbole1003@users.noreply.github.com>
2020-07-29 16:34:35 +0530

    Create Part_1.py

---
+++ Part_1.py
@@ -0,0 +1,12 @@
+#To check if the given year is a leap year.
+def test_year(year): 
+    if (year % 4) == 0: 
+        if (year % 100) == 0: 
+            if (year % 400) == 0: 
+                return True
+            else: 
+                return False
+        else: 
+             return True
+    else: 
+        return False
