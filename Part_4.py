d780b0b1117ba98910dcf3a3f8dca10d7a144464
Shreya Godbole <65806185+shreyagodbole1003@users.noreply.github.com>
2020-07-29 16:37:58 +0530

    Create Part_4.py

---
+++ Part_4.py
@@ -0,0 +1,7 @@
+#To test if the given letter is a vowel or a consonant.
+def test_vowel(x): 
+    if (x=='a' or x=='A' or x=='E' or x=='e' or
+        x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U' ): 
+        return True
+    else: 
+        return False
